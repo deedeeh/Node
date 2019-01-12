a744efa2e2b047428e43e8882af03fc6856600d8
deedeeh <dinahafez.8@gmail.com>
2019-01-12 18:35:49 +0000

    Added routes parameters in express

--- FirstExpressApp/app.js
+++ FirstExpressApp/app.js
@@ -2,7 +2,17 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+//order of routes is important so the '*' one should be at the end.
 app.get('/', (req, res) => res.send('Hello World'))
 app.get('/bye', (req, res) => res.send('Goodbye!'))
 app.get('/cat', (req, res) => res.send('MEOW!'))
+
+//Route parameters, they start with colon ':' 
+app.get('/r/:redditName/', (resp, res) => {
+  return res.send(`I am a ${resp.params.redditName} reddit`)
+})
+
+//can be used for error messages for routes not defined.
+app.get('*', (req, res) => res.send('Sorry no route by this name!'))
+
 app.listen(port)
