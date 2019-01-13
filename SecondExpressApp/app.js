18278deba19356490738b6273dbca58c99a95e3e
deedeeh <dinahafez.8@gmail.com>
2019-01-13 14:37:01 +0000

    Added a better solution for animals route

--- SecondExpressApp/app.js
+++ SecondExpressApp/app.js
@@ -3,25 +3,39 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => res.send('Hi there, welcome to my assignment!'))
+
+//More efficient way of doing the animals route.
 app.get('/speak/:animal', (req, res) => {
-  switch(req.params.animal) {
-    case 'pig':
-      res.send('Oink')
-      break;
-    case 'cow':
-      res.send('Moo')
-      break;
-    case 'dog':
-      res.send('Woof Woof!')
-      break;
-    default:
-      res.send('Sorry, cannot find your animal!')
+  const sounds = {
+    pig: 'Oink',
+    cow: 'Moo',
+    dog: 'Woof Woof!',
+    cat: 'MEOW'
   }
+  const animal = req.params.animal.toLowerCase()
+  res.send(`The ${animal} says "${sounds[animal]}"`)
 })
+
+//MY FIRST SOLUTION
+// app.get('/speak/:animal', (req, res) => {
+//   switch(req.params.animal.toLowerCase()) {
+//     case 'pig':
+//       res.send('Oink')
+//       break;
+//     case 'cow':
+//       res.send('Moo')
+//       break;
+//     case 'dog':
+//       res.send('Woof Woof!')
+//       break;
+//     default:
+//       res.send('Sorry, cannot find your animal!')
+//   }
+// })
 
 app.get('/repeat/:word/:num', (req, res) => {
   const word = req.params.word + ' '
-  const repeatedWord = word.repeat(req.params.num)
+  const repeatedWord = word.repeat(Number(req.params.num))
   return res.send(repeatedWord)
 })
 
