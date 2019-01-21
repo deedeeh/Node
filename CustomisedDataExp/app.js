8805453ef10e4c8868416b79b138cc7d83b9e72d
deedeeh <dinahafez.8@gmail.com>
2019-01-21 23:56:52 +0000

    Added views dir and partials, 2 pages until now are built

--- CustomisedDataExp/app.js
+++ CustomisedDataExp/app.js
@@ -1,8 +1,21 @@
 const express = require('express')
 const app = express()
 
+app.set('view engine', 'ejs')
+
 app.get('/', (req, res) => {
-  res.send('Hello from the other side!')
+  res.render('index')
+})
+
+capitalize = (s) => {
+  return s.charAt(0).toUpperCase() + s.slice(1)
+}
+
+app.get('/customise/:name/:jobTitle/:interest', (req, res) => {
+  const name = capitalize(req.params.name)
+  const jobTitle = req.params.jobTitle
+  const interest = req.params.interest
+  res.render('customise', {custName: name, custJob: jobTitle, custInterest: interest})
 })
 
 app.listen(3000, console.log('Server is running!'))
