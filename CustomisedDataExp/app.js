f7e3155ee65f1e7640027eaf4757b8f7a8c49f9f
deedeeh <dinahafez.8@gmail.com>
2019-01-22 23:31:11 +0000

    Added lowercase to the data except name

--- CustomisedDataExp/app.js
+++ CustomisedDataExp/app.js
@@ -15,15 +15,23 @@ app.get('/customise/:name/:jobTitle/:interest', (req, res) => {
   const name = req.params.name
   const jobTitle = req.params.jobTitle
   const interest = req.params.interest
-  res.render('customise', {custName: wordsChecker(capitalize(name)), custJob: wordsChecker(jobTitle), custInterest: wordsChecker(interest)})
+  res.render('customise', {custName: wordsCheckerForName(capitalize(name)), custJob: wordsCheckerForRest(jobTitle), custInterest: wordsCheckerForRest(interest)})
 
 })
 
 // trying to check if the input is more than one word, camelCase, I need to split it and display it the user 
-wordsChecker = (s) => {
+//because I want the name to be capitalised 
+wordsCheckerForName = (s) => {
   return s.split('').map(char => {
     return char === char.toUpperCase() ? ' ' + char : char
   }).join('')
 }
+
+//I want any other data to be LowerCase and if I try to do it in the object it just removes the space between words.
+wordsCheckerForRest = (s) => {
+    return s.split('').map(char => {
+      return char === char.toUpperCase() ? ' ' + char.toLowerCase() : char
+    }).join('')
+  }
 
 app.listen(3000, console.log('Server is running!'))
