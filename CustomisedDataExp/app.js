8b99f2e64c0b70e35298648d77cd55abacff0222
deedeeh <dinahafez.8@gmail.com>
2019-01-27 01:35:53 +0000

    Added some styling to the pages except customiser.ejs

--- CustomisedDataExp/app.js
+++ CustomisedDataExp/app.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser')
 const app = express()
 
 app.set('view engine', 'ejs')
+app.use('/public', express.static('public'))
 
 const urlencodedParser = bodyParser.urlencoded({extended: false})
 
