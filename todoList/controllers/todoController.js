ee27f3e31eaa657c85466b165ca54fc2440842c3
deedeeh <dinahafez.8@gmail.com>
2019-02-02 21:18:34 +0000

    Added new todos with post req and delete them with delete req

--- todoList/controllers/todoController.js
+++ todoList/controllers/todoController.js
@@ -1,18 +1,29 @@
+const bodyParser = require('body-parser')
+const urlencodedParser = bodyParser.urlencoded({extended: false});
+
+let data = [
+  {item: 'prepare food'},
+  {item: 'feed cat'},
+  {item: 'practice node.js'}
+];
+
 module.exports = function(app) {
 
   //get a todo 
   app.get('/todo', (req, res) => {
-    res.render('todo');
+    res.render('todo', {todos: data});
   });
 
   //create a todo
-  app.post('/todo', (req, res) => {
-
+  app.post('/todo', urlencodedParser, (req, res) => {
+    data.push(req.body);
+    res.json(data);
   });
 
   //delete a todo
-  app.delete('/todo', (req, res) => {
-
+  app.delete('/todo/:item', (req, res) => {
+    data = data.filter(todo => todo.item.replace(/ /g, '-') !== req.params.item);
+    res.json(data);
   });
 
 
