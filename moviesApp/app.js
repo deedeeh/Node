40c86c32609b4e71f9cfd5b4052cf5b49f090793
deedeeh <dinahafez.8@gmail.com>
2019-02-07 21:35:02 +0000

    Created a new url with a search bar that directs to the results url

--- moviesApp/app.js
+++ moviesApp/app.js
@@ -5,8 +5,13 @@ const API_key = require('./config');
 
 app.set('view engine', 'ejs')
 
+app.get('/', (req, res) => {
+  res.render('search');
+});
+
 app.get('/results', (req, res) => {
-  request(`http://www.omdbapi.com/?apikey=${API_key}&s=potter`, (error, response, body) => {
+  const query = req.query.search;
+  request(`http://www.omdbapi.com/?apikey=${API_key}&s=${query}`, (error, response, body) => {
     if(!error && response.statusCode == 200) {
       const parsedData = JSON.parse(body);
       res.render('results', {movies: parsedData})
